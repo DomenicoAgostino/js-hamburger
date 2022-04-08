99d362046f15dd566bc98d761d35c3915464180e
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-08 15:56:39 +0200

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ console.log("hello world");
 
 const menuHamburger = document.querySelector(".header-right > a")
 
+
+menuHamburger.addEventListener("click", function() {
+  
+  
+  const clickMenu = document.querySelector (".hamburger-menu");
+  clickMenu.classList.add("active")
+
+});
+
+
+const closeHamburger = document.querySelector(".close");
+
+
+closeHamburger.addEventListener("click", function() {
+
+  const clickMenu = document.querySelector (".hamburger-menu");
+  clickMenu.classList.remove("active");
+})
